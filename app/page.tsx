35a3f0abe5f9c71ad21e30268b5c16ec86a35168
heyyakash <akashsharma2002@gmail.com>
2024-12-21 17:09:17 +0530

    removed unwanted imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import DashedLine from "@/components/DashedLine";
 import Hero from "@/components/Hero";
 import Introduction from "@/components/introduction";
-import Projects from "@/components/projects";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
