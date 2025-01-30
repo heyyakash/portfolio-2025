e54d4a3c01d1dfb49e8ea59d9d140daf98bfd14a
heyyakash <akashsharma2002@gmail.com>
2025-01-30 22:00:54 +0530

    changed pfp

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -7,7 +7,7 @@ const Introduction = () => {
             <div className='max-w-[1200px] w-full mx-auto my-5 text-3xl px-4  font-semibold m'>Intro</div>
             <div className='grid grid-cols-1 grid-rows-2 md:grid-cols-3 md:grid-rows-1 max-w-[1200px] w-full px-4  md:gap-6 items-center place-items-center mx-auto'>
                 <CustomCard className='col-span-1 w-full h-full'>
-                    <img src="/check.webp" className='w-full h-full  object-cover  grayscale hover:grayscale-0 trans cursor-pointer' alt="avatar" />
+                    <img src="/profile.jpeg" className='w-full h-full  object-cover  grayscale hover:grayscale-0 trans cursor-pointer' alt="avatar" />
                 </CustomCard>
                 <div className='col-span-2 grid grid-cols-1 mt-8 md:mt-0 grid-rows-3 gap-4 h-full'>
                     <CustomCard className='w-full flex items-center justify-center p-6'>
